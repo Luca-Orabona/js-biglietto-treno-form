2e1e94866a1c31818d21acfd5856925e27b1bb4a
Luca-Orabona <lucaorabona94@gmail.com>
2025-04-29 15:21:16 +0200

    feat: MILESTONE 1 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,56 @@
+// elementi input DOM
+const kmElem = document.getElementById("km");
+
+const etaElem = document.getElementById("età");
+
+const FormElem = document.getElementById("form");
+
+
+
+
+FormElem.addEventListener("submit", handlefrom);
+
+function handlefrom(event) {
+    event.preventDefault();
+
+// valori elementi input DOM
+const km = kmElem.value;
+
+const eta = etaElem.value;
+
+
+
+/* ESECUZIONE LOGICA */
+
+const totalPrice = km * 0.21;
+
+let result;
+
+if (eta < 18) {
+    const discount20 = (totalPrice * 20) / 100;
+    
+    result = totalPrice - discount20;
+    
+} else if (eta >= 65) {
+    const discount40 = (totalPrice * 40) / 100;
+
+    result = totalPrice - discount40; 
+
+} else {
+    result = totalPrice;
+    
+}
+
+
+/* OUTPUT */
+
+const message = `Il prezzo del biglietto è ${result.toFixed(2)}€`;
+console.log(message);
+
+
+// resetto i campi
+FormElem.reset();
+
+}
+
 
