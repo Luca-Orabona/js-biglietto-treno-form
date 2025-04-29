c4faa6767402e7a190ddc30f76da9744941a4fe4
Luca-Orabona <lucaorabona94@gmail.com>
2025-04-29 17:43:31 +0200

    feat: MILESTONE 2 completed

--- js/script.js
+++ js/script.js
@@ -3,20 +3,38 @@ const kmElem = document.getElementById("km");
 
 const etaElem = document.getElementById("età");
 
+const fullName = document.getElementById("full-name");
+
 const FormElem = document.getElementById("form");
 
 
 
+// elementi della card da compilare
+const nameUser = document.querySelector("#card-body-passeggero p")
 
+const prezzoBiglietto = document.getElementById("costo-biglietto");
+
+const numCarrozza = document.getElementById("num-carrozza");
+
+const codiceCP = document.getElementById("codice-cp");
+
+const sconto = document.getElementById("offerta")
+
+
+/******** **********/
 FormElem.addEventListener("submit", handlefrom);
 
 function handlefrom(event) {
     event.preventDefault();
 
+
+
 // valori elementi input DOM
 const km = kmElem.value;
 
 const eta = etaElem.value;
+
+const fullNameUser = fullName.value;
 
 
 
@@ -30,15 +48,15 @@ if (eta < 18) {
     const discount20 = (totalPrice * 20) / 100;
     
     result = totalPrice - discount20;
-    
+    sconto.innerHTML = "20%";
 } else if (eta >= 65) {
     const discount40 = (totalPrice * 40) / 100;
 
     result = totalPrice - discount40; 
-
+    sconto.innerHTML = "40%";
 } else {
     result = totalPrice;
-    
+    sconto.innerHTML = "-";
 }
 
 
@@ -46,6 +64,18 @@ if (eta < 18) {
 
 const message = `Il prezzo del biglietto è ${result.toFixed(2)}€`;
 console.log(message);
+
+
+
+// stampo i valore in pagina
+nameUser.innerHTML = fullNameUser;
+
+prezzoBiglietto.innerHTML = `${result.toFixed(2)}€`;
+
+numCarrozza.innerHTML = Math.floor(Math.random() * 15) + 1;
+
+codiceCP.innerHTML = Math.floor(Math.random() * 100000) + 10000;
+
 
 
 // resetto i campi
